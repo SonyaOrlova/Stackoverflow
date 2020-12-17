8d0db53fe289dd095b85eb3d6c412c121e1e3ca4
Sonya Orlova <sonya5488@yandex.ru>
2020-12-17 19:08:56 +0300

    Изменение языка описания тестов.

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
 
   describe 'POST#create' do
-    context 'с валидными параметрами' do
-      it 'добавляет новый ответ в базу данных и возвращает на страницу созданного ответа' do
+    context 'valid attributes' do
+      it 'save a new answer and redirects to show view' do
         expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
         expect(response).to redirect_to assigns(:exposed_answer)
       end
     end
 
-    context 'с невалидными параметрами' do
-      it 'не добавляет новый ответ в базу данных и возвращает на страницу создания нового ответа' do
+    context 'invalid attributes' do
+      it 'does not save a new answer and renders new template' do
         expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(question.answers, :count)
         expect(response).to render_template :new
       end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe QuestionsController, type: :controller do
   let(:question) { create(:question) }
 
   describe 'POST#create' do
-    context 'с валидными параметрами' do
-      it 'добавляет новый вопрос в базу данных и возвращает на страницу созданного вопроса' do
+    context 'valid attributes' do
+      it 'save a new question and redirects to show view' do
         expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
         expect(response).to redirect_to assigns(:exposed_question)
       end
     end
 
-    context 'с невалидными параметрами' do
-      it 'не добавляет новый вопрос в базу данных и возвращает на страницу создания нового вопроса' do
+    context 'invalid attributes' do
+      it 'does not save a new question and renders new template' do
         expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
         expect(response).to render_template :new
       end
