ce55e973d0a936d4d8d529cacf64f009123d6978
Sonya Orlova <sonya5488@yandex.ru>
2020-12-28 21:21:06 +0300

    Корректировка ассоциаций модели пользователя и созданных им вопросов и ответов.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,7 +12,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = current_user.created_questions.new(question_params)
+    @question = current_user.questions.new(question_params)
 
     if @question.save
       redirect_to @question, notice: 'Question was successfuly created'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, :recoverable
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  has_many :created_questions, foreign_key: 'author_id', class_name: 'Question', dependent: :destroy
-  has_many :created_answers, foreign_key: 'author_id', class_name: 'Answer', dependent: :destroy
+  has_many :questions, foreign_key: 'author_id', dependent: :destroy
+  has_many :answers, foreign_key: 'author_id', dependent: :destroy
 
   def author?(record)
     id == record.author_id
